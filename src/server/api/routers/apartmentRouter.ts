9262287e67e291cce6b17b32557acc06632f3102
Laden <delfanladen24@gmail.com>
2025-06-06 23:05:00 +0800

    refactor: remove mocked DB and clean up apartmentRouter code

--- src/server/api/routers/apartmentRouter.ts
+++ src/server/api/routers/apartmentRouter.ts
@@ -1,18 +1,7 @@
 import { z } from "zod";
 import { authProcedure, createTRPCRouter, publicProcedure } from "../trpc";
 import { createClient } from "@/utils/supabase/server";
-
-// Mocked DB
-interface Post {
-  id: number;
-  name: string;
-}
-const posts: Post[] = [
-  {
-    id: 1,
-    name: "Hello World",
-  },
-];
+import { create } from "domain";
 
 export const apartmentRouter = createTRPCRouter({
   list: authProcedure.query(async () => {
@@ -25,4 +14,63 @@ export const apartmentRouter = createTRPCRouter({
     if (error) throw new Error(error.message);
     return data;
   }),
+  create: authProcedure
+    .input(
+      z.object({
+        name: z.string().min(1, "Name is required"),
+        description: z.string().optional(),
+        address: z.string().min(1, "Address is required"),
+        gmaps: z.string().url("Invalid Google Maps URL"),
+        electric_number: z.string().optional(),
+        water_number: z.string().optional(),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const supabase = await createClient();
+      const { data, error } = await supabase
+        .from("apartments")
+        .insert({
+          user_id: ctx.supabaseUser?.id,
+          name: input.name,
+          description: input.description,
+          address: input.address,
+          gmaps: input.gmaps,
+          electric_number: input.electric_number,
+          water_number: input.water_number,
+          created_at: new Date().toISOString(),
+        })
+        .select();
+      if (error) throw new Error(error.message);
+      return data;
+    }),
+  update: authProcedure
+    .input(
+      z.object({
+        id: z.string().min(1, "ID is required"),
+        name: z.string().min(1, "Name is required"),
+        description: z.string().optional(),
+        address: z.string().optional(),
+        gmaps: z.string().url("Invalid Google Maps URL").optional(),
+        electric_number: z.string().optional(),
+        water_number: z.string().optional(),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const supabase = await createClient();
+      const { data, error } = await supabase
+        .from("apartments")
+        .update({
+          name: input.name,
+          description: input.description,
+          address: input.address,
+          gmaps: input.gmaps,
+          electric_number: input.electric_number,
+          water_number: input.water_number,
+        })
+        .eq("id", input.id)
+        .select();
+      if (error) throw new Error(error.message);
+      return data;
+    }),
+
 });
