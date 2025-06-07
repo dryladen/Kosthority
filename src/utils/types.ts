adcd2ec9c9c9f1db40e253152a3b96efc2df208e
Laden <delfanladen24@gmail.com>
2025-06-07 18:54:25 +0800

    feat: add Apartment type definition to types utility

---
+++ src/utils/types.ts
@@ -0,0 +1,9 @@
+export type Apartment = {
+    id: string;
+    name: string;
+    description?: string;
+    address: string;
+    gmaps: string;
+    electric_number?: string;
+    water_number?: string;
+};
