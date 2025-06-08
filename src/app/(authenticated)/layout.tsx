59c616959d1c09c26ed976318f506feeb602eb13
Laden <delfanladen24@gmail.com>
2025-06-08 14:18:30 +0800

    fix: correct formatting and uncomment user authentication logic in Layout component

--- src/app/(authenticated)/layout.tsx
+++ src/app/(authenticated)/layout.tsx
@@ -1,4 +1,4 @@
-import { SidebarProvider} from "@/components/ui/sidebar";
+import { SidebarProvider } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/app-sidebar";
 import AppNavbar from "@/components/app-navbar";
 import { createClient } from "@/utils/supabase/server";
@@ -6,11 +6,11 @@ import { redirect } from "next/navigation";
 
 
 export default async function Layout({ children }: { children: React.ReactNode }) {
-  // const supabase = await createClient();
-  // const { data, error } = await supabase.auth.getUser()
-  // if (error || !data?.user) {
-  //   redirect('/sign-in')
-  // }
+  const supabase = await createClient();
+  const { data, error } = await supabase.auth.getUser()
+  if (error || !data?.user) {
+    redirect('/sign-in')
+  }
   return (
     <SidebarProvider>
       <AppSidebar email={"Username"} />
